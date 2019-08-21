8dbaba40dcb895b2853015343540a02218314fea
Chan Yeoh <chanyeoh@Chans-iMac.fios-router.home>
2019-08-20 22:28:47 -0400

    Data Transformation refactored

--- Data Transformation/data_transformation.py
+++ Data Transformation/data_transformation.py
@@ -12,104 +12,117 @@ class DataPoint:
 		else:
 			self.classification = 0
 
+	def plot(self, plt):
+		if self.classification == 1:
+			plt.plot(self.x, self.y, 'ro')
+		else:
+			plt.plot(self.x, self.y, 'bx')
+
 class DataPoint3D(DataPoint):
 	def __init__(self, x, y, z, classification):
 		super().__init__(x, y, classification)
 		self.z = z
 
-def circle_calculation(x_arr, y_arr):
+	def plot(self, plt):
+		if self.classification == 1:
+			plt.scatter(self.x, self.y, self.z, marker='o')
+		else:
+			plt.scatter(self.x, self.y, self.z, marker='x')
+
+class ConeTransformation:
+	C = 1
+	def __init__(self, data_dp):
+		self.data_dp = data_dp
+
+	def transform_to_cone(self):
+		cone_data = []
+		for dp in self.data_dp:
+			cone_dp = self.calculate_cone_data_point(dp)
+			cone_data.append(cone_dp)
+		return cone_data
+
+	def calculate_cone_data_point(self, dp):
+		z = math.sqrt(self.C * pow(dp.x, 2) + self.C * pow(dp.y, 2))
+		return DataPoint3D(dp.x, dp.y, z, dp.classification)
+
+
+	def get_x_y_point(self, cone_data):
+		data = []
+		for dp in cone_data:
+			data.append(DataPoint(dp.x, dp.y, dp.classification))
+		return data
+
+	def get_x_z_point(self, cone_data):
+		data = []
+		for dp in cone_data:
+			data.append(DataPoint(dp.x, dp.z, dp.classification))
+		return data
+
+	def get_y_z_point(self, cone_data):
+		data = []
+		for dp in cone_data:
+			data.append(DataPoint(dp.y, dp.z, dp.classification))
+		return data
+
+	def get_x_point(self, cone_data):
+		data = []
+		for dp in cone_data:
+			data.append(DataPoint(dp.x, 0, dp.classification))
+		return data
+
+	def get_y_point(self, cone_data):
+		data = []
+		for dp in cone_data:
+			data.append(DataPoint(dp.y, 0, dp.classification))
+		return data
+
+	def get_z_point(self, cone_data):
+		data = []
+		for dp in cone_data:
+			data.append(DataPoint(dp.z, 0, dp.classification))
+		return data
+
+
+RADIUS = 1
+def generate_circle_data():
 	data = []
-	RADIUS = 1
+	x_arr = np.arange(-2., 2., 0.2)
+	y_arr = np.arange(-2., 2., 0.2)
 	for x in x_arr:
 		for y in y_arr:
-			r = pow(x, 2) + pow(y, 2)
-			dp = DataPoint(x, y, r < RADIUS)
+			dp = calculate_and_label_radius(x, y)
 			data.append(dp)
 	return data
 
+def calculate_and_label_radius(x, y):
+	r = pow(x, 2) + pow(y, 2)
+	return DataPoint(x, y, r < RADIUS)
+
 def plot_data(data):
 	plt.figure()
-	for d in data:
-		if d.classification == 1:
-			plt.plot(d.x, d.y, 'ro')
-		else:
-			plt.plot(d.x, d.y, 'bx')
 	plt.xlim([-4,4])
 	plt.ylim([-4,4])
+	for d in data:
+		d.plot(plt)
+
 	plt.show()
 
 def plot_3d_data(data):
 	fig = plt.figure()
 	ax = fig.add_subplot(111, projection='3d')
-
 	for dp in data:
-		if dp.classification == 1:
-			ax.scatter(dp.x, dp.y, dp.z, marker='o')
-		else:
-			ax.scatter(dp.x, dp.y, dp.z, marker='x')
-
-	plt.show()
-
-def cone_transformation(data):
-	cone_data = []
-	C = 1
-	for dp in data:
-		z = math.sqrt(C * pow(dp.x, 2) + C * pow(dp.y, 2))
-		dp_z = DataPoint3D(dp.x, dp.y, z, dp.classification)
-		cone_data.append(dp_z)
-	return cone_data
-
-def flatten_transformation(data):
-	# Only get Z and X
-	plt.figure()
-	for d in data:
-		if d.classification == 1:
-			plt.plot(d.x, d.z, 'ro')
-		else:
-			plt.plot(d.x, d.z, 'bx')
-	plt.xlim([-4,4])
-	plt.ylim([-4,4])
-	plt.show()
-	# Only get Z and Y
-	plt.figure()
-	for d in data:
-		if d.classification == 1:
-			plt.plot(d.y, d.z, 'ro')
-		else:
-			plt.plot(d.y, d.z, 'bx')
-	plt.xlim([-4,4])
-	plt.ylim([-4,4])
-	plt.show()
-	# Only get X and Y
-	plt.figure()
-	for d in data:
-		if d.classification == 1:
-			plt.plot(d.x, d.y, 'ro')
-		else:
-			plt.plot(d.x, d.y, 'bx')
-	plt.xlim([-4,4])
-	plt.ylim([-4,4])
-	plt.show()
-
-def super_flat(data):
-	# Only get Z and X
-	plt.figure()
-	for d in data:
-		if d.classification == 1:
-			plt.plot(0, d.z, 'ro')
-		else:
-			plt.plot(0, d.z, 'bx')
-	plt.xlim([-4,4])
-	plt.ylim([-4,4])
+		dp.plot(ax)
 	plt.show()
 
 
-x = np.arange(-2., 2., 0.2)
-y = np.arange(-2., 2., 0.2)
+if __name__ == "__main__":
+	# Data Transformation
+	data = generate_circle_data()
+	cone_transformation = ConeTransformation(data)
+	cone_3d_dp = cone_transformation.transform_to_cone()
 
-data = circle_calculation(x, y)
-plot_data(data)
-cone_data = cone_transformation(data)
-plot_3d_data(cone_data)
-flatten_transformation(cone_data)
-super_flat(cone_data)
+	# Plot the Data
+	plot_data(data)
+	plot_3d_data(cone_3d_dp)
+	plot_data(cone_transformation.get_x_z_point(cone_3d_dp))
+	plot_data(cone_transformation.get_z_point(cone_3d_dp))
